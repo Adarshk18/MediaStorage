18c51a014ca573b301d3106cb005a2c5faa445d7
Adarshk18 <adarshkohli183@gmail.com>
2025-08-21 08:48:42 +0530

    new

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,23 @@ use std::fs;
 use std::io::Error;
 
 fn main(){
-    let text = fs::read_to_string("logs.txt");
+    // let text = fs::read_to_string("logs.txt");
 
-    println!("{:#?}",text);
+    // println!("{:#?}",text);
+
+    match divide(5.0, 0.0) {
+        Ok(result_of_division) =>{
+            println!("{}",result_of_division);
+        }
+        Err(what_went_wrong) =>{
+            println!("{}",what_went_wrong);
+        }
+    }
+
+    
 }
 
-fun divide(a: f64, b: f64) -> Result<f64,Error>{
+fn divide(a: f64, b: f64) -> Result<f64,Error>{
     if b== 0.0{
         Err(Error::other("cant divide by 0"))
     }else{
