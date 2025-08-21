1d4a158c55c79701a0381c4f94ac775c0ddde8a6
Adarshk18 <adarshkohli183@gmail.com>
2025-08-21 09:24:57 +0530

    new

---
+++ src/using_results_when_readingfiles.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn string_test (a: String, b: &String, c: &str){
+
+}
+
+fn main() {
+
+    string_test(
+        String::from("red"),
+        &String::from("red"),
+        String::from("red").as_str()
+
+    );
+
+
+    match fs::read_to_string("logs.txt"){
+        Ok(text_that_was_read) => println!("{}",text_that_was_read.len()),
+        Err(why_failed) => println!("failed to read file : {}",why_failed),
+    }
+}
